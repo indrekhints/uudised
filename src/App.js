00f4bc9a11b42a0fc460affdb93ae0f929a58af3
indrek hints <indrek.hints@mail.ee>
2023-12-05 19:24:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="home/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="home/:UudisId" element={<Uudis />} />
         </Routes>
